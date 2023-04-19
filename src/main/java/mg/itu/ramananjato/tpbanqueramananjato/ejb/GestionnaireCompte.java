3cd4cad9febe7a86a3393130af898c6e00edd3b8
hemmy3103 <ramananjatohemmy@gmail.com>
2023-04-19 20:38:27 +0300

    Making a deposit and withdrawal

--- src/main/java/mg/itu/ramananjato/tpbanqueramananjato/ejb/GestionnaireCompte.java
+++ src/main/java/mg/itu/ramananjato/tpbanqueramananjato/ejb/GestionnaireCompte.java
@@ -118,4 +118,25 @@ public class GestionnaireCompte {
             return "";
         }
     }
+    
+     /**
+     * Dépôt d'argent sur un compte bancaire.
+     * @param compteBancaire
+     * @param montant 
+     */
+    public void deposer(CompteBancaire compteBancaire, int montant) {
+      compteBancaire.deposer(montant);
+      update(compteBancaire);
+    }
+    
+    /**
+     * Retrait d'argent sur un compte bancaire.
+     * @param compteBancaire
+     * @param montant 
+     */
+    public void retirer(CompteBancaire compteBancaire, int montant) {
+      compteBancaire.retirer(montant);
+      update(compteBancaire);
+    }
+    
 }
